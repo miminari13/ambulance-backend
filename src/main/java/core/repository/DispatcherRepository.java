35f9f2fdfbef2a89895155780877d0495adfa627
Maria Art <miminari13@gmail.com>
2022-05-27 18:46:07 +0300

    Added dispatchers list api

---
+++ src/main/java/core/repository/DispatcherRepository.java
@@ -0,0 +1,7 @@
+package core.repository;
+
+import core.model.Dispatcher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DispatcherRepository extends CrudRepository<Dispatcher, Integer> {
+}
